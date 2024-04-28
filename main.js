743618e606d3e796517514f217318be6d5ed1c75
thais maciel <thaismaciielf@hotmail.com>
2024-04-28 00:03:58 -0300

    commit inicial de projeto básico com jquery

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() { 
+        $('form').slideDown(550);
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp(550);
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none"> </li>');
+        $(`<img src='${enderecoDaNovaImagem}'/>`).appendTo(novoItem);
+        $(
+            `<div class='overlay-imagem-link'>
+            <a href='${enderecoDaNovaImagem} target='_blanck' title='Ver imagem em tamanho real'>
+            Ver imagem em tamanho real 
+            </a>
+            </div>`
+            ).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
+    })
+})
